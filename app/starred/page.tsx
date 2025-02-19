975372b70e0d639b0e2fa8353e8d1f52feac5d11
Melvin <melvinmps11301@gmail.com>
2025-02-19 13:50:02 +0300

    starred page created

---
+++ app/starred/page.tsx
@@ -0,0 +1,9 @@
+import SatrredPage from "@/components/pages/starred-page";
+
+export default function Home() {
+  return (
+    <div>
+      <SatrredPage />
+    </div>
+  );
+}
---
+++ components/pages/starred-page.tsx
@@ -0,0 +1,7 @@
+export default function StarredPage() {
+  return (
+    <div>
+      <h1>Starred Page</h1>
+    </div>
+  );
+}
